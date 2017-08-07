e1152103702e8f44ee6df3d8d3cfc00251145f89
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-07 14:39:12 -0700

    Copying over

--- test.js
+++ test.js
@@ -1,12 +1,88 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import tempLate from "./"
+import mergeDeepRight from "./"
 
 test(({same, end}) => {
   same(
-    tempLate(true),
-    false
+    mergeDeepRight({alpha: "1"})({beta: "2"}),
+    {
+      alpha: "1",
+      beta: "2",
+    }
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    mergeDeepRight({gamma: {alpha: "1"}})({gamma: {beta: "2"}}),
+    {
+      gamma: {
+        alpha: "1",
+        beta: "2",
+      }
+    }
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    mergeDeepRight({alpha: {alpha: "1"}})({beta: {beta: "2"}}),
+    {
+      alpha: {alpha: "1"},
+      beta: {beta: "2"}
+    }
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    mergeDeepRight({alpha: {alpha: "1"}})({alpha: {alpha: "2"}}),
+    {alpha: {alpha: "2"}}
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    mergeDeepRight(["a"])(["b"]),
+    [
+      "a",
+      "b",
+    ]
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    mergeDeepRight(["a"])(["a"]),
+    [
+      "a",
+      "a",
+    ]
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    mergeDeepRight({alpha: ["a"]})({alpha: ["a"]}),
+    {
+      alpha: [
+        "a",
+        "a",
+      ]
+    }
   )
 
   end()
