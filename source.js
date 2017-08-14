2cd01dc6cde2a45d9f49be0c115428cc303a8b76
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-13 23:32:39 -0700

    Fixing issues with typing and usage of ramda

--- source.js
+++ source.js
@@ -1,4 +1,4 @@
-import {concat} from "ramda"
+import mergeRight from "@unction/mergeright"
 import mergeWith from "@unction/mergewith"
 import isObject from "@unction/isobject"
 import isArray from "@unction/isarray"
@@ -6,7 +6,7 @@ import isArray from "@unction/isarray"
 export default function mergeDeepRight (left: IterableType): Function {
   return function mergeDeepRightRight (right: IterableType): IterableType {
     if (isArray(left) && isArray(right)) {
-      return concat(left)(right)
+      return mergeRight(left)(right)
     }
 
     if (isObject(left) && isObject(right)) {
