d593169802fbd055a10be8f259bec8c7e5128eeb
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:32:36 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import mergeRight from "@unction/mergeright"
-import mergeWith from "@unction/mergewith"
-import isObject from "@unction/isobject"
-import isArray from "@unction/isarray"
-
-export default function mergeDeepRight (left: FunctorType): Function {
-  return function mergeDeepRightRight (right: FunctorType): FunctorType {
+import mergeRight from "@unction/mergeright";
+import mergeWith from "@unction/mergewith";
+import isObject from "@unction/isobject";
+import isArray from "@unction/isarray";
+export default function mergeDeepRight(left) {
+  return function mergeDeepRightRight(right) {
     if (isArray(left) && isArray(right)) {
-      return mergeRight(left)(right)
+      return mergeRight(left)(right);
     }
 
     if (isObject(left) && isObject(right)) {
-      return mergeWith(mergeDeepRight)(left)(right)
+      return mergeWith(mergeDeepRight)(left)(right);
     }
 
-    return right
-  }
+    return right;
+  };
 }
